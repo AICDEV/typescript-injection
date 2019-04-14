c682a8d16a6dc5c7c4610eca2473ce73513b1a0b
AICDEB <jens.schmidt@take-smart.com>
2019-04-14 13:13:37 +0200

    add decorator example

---
+++ src/database/_decorators/DatabaseDecorator.ts
@@ -0,0 +1,12 @@
+import { IDatabaseOperations } from "../interfaces";
+import {DatabaseContainer, DATABASETYPES} from "../container";
+
+function injectDabaseService(type: string) {
+    return function injectMongoOperations<T extends new (...args: any[]) => {}>(constructor: T) {
+        return class extends constructor {
+          // tslint:disable-next-line:member-access
+          database: IDatabaseOperations = DatabaseContainer.getTagged<IDatabaseOperations>(DATABASETYPES.IDatabaseOperations, DATABASETYPES.DatabaseType, type);
+        };
+      }
+}
+export { injectDabaseService};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { IDatabaseOperations } from "./database/interfaces";
 import { DatabaseContainer, DATABASETYPES } from "./database/container";
 import { ICar } from "./database/interfaces/ICar";
+import { injectDabaseService} from "./database/_decorators";
 
 const databaseOne: IDatabaseOperations = DatabaseContainer.getTagged<IDatabaseOperations>(DATABASETYPES.IDatabaseOperations, DATABASETYPES.DatabaseType, "mongo");
 databaseOne.read<ICar>("car").then((result) => {
@@ -12,3 +13,18 @@ databaseTwo.read<ICar>("car").then((result) => {
     console.log(result);
 });
 
+/**
+ * inject mongo operations to class
+ */
+@injectDabaseService("mongo")
+class Car {
+    private database!: IDatabaseOperations;
+
+    public async read(): Promise<void> {
+        const result = await this.database.read<ICar>("car");
+        console.log(result);
+    }
+}
+
+const car = new Car();
+car.read();
