ac9ef1f5dca55522cd406973e2293bd5c276c9d1
AICDEB <jens.schmidt@take-smart.com>
2019-04-21 09:29:26 +0200

    clean namespace

--- src/database/_decorators/DatabaseDecorator.ts
+++ src/database/_decorators/DatabaseDecorator.ts
@@ -2,7 +2,7 @@ import { IDatabaseOperations } from "../interfaces";
 import {DatabaseContainer, DATABASETYPES} from "../container";
 
 function injectDabaseService(type: string) {
-    return function injectMongoOperations<T extends new (...args: any[]) => {}>(constructor: T) {
+    return function inject<T extends new (...args: any[]) => {}>(constructor: T) {
         return class extends constructor {
           // tslint:disable-next-line:member-access
           database: IDatabaseOperations = DatabaseContainer
